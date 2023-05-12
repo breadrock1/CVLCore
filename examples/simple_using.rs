027c8eb3b862e63ac5a5743c1c05c00acd01b647
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-05-12 21:40:04 +0300

    Realized new functionalities into lib.rs:  - Realize computing vibrating pixels with neighbours for passed frame;  - Realize setting colors by stored values into Point;  - Realize computing median of Mat values;  - Add supporting of all changes.

--- examples/simple_using.rs
+++ examples/simple_using.rs
@@ -3,13 +3,14 @@ use crate::cvldetector::cvldetector::*;
 
 use opencv::prelude::VideoCaptureTrait;
 use opencv::{core, highgui, prelude, videoio};
+use opencv::core::Mat;
 
 fn main() {
     let mut maxval = 150.0;
     let mut thresh1 = 150.0;
     let mut thresh2 = 100.0;
 
-    //let mut frames_to_abs: Vec<Box<core::Mat>> = Vec::new();
+    let mut frames_to_abs: Vec<&Mat> = Vec::new();
 
     highgui::named_window("Simple using", highgui::WINDOW_FULLSCREEN).unwrap();
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap();
@@ -19,15 +20,23 @@ fn main() {
         cam.read(&mut frame).unwrap();
 
         let gray_frame = gen_grayscale_frame(&frame).unwrap();
-        let thresh_frame = gen_threshold_frame(&gray_frame, thresh1, maxval).unwrap();
+        // let thresh_frame = gen_threshold_frame(&gray_frame, thresh1, maxval).unwrap();
         // let canny_frame = gen_canny_frame(&thresh_frame, thresh1, thresh2, 5, true).unwrap();
-        let mut canny_frame = gen_canny_frame_by_sigma(&thresh_frame, 5, 0.33, true).unwrap();
+        // let mut canny_frame = gen_canny_frame_by_sigma(&thresh_frame, 5, 0.33, true).unwrap();
+        let mut canny_frame = gen_canny_frame_by_sigma(&gray_frame, 3, 0.01, true).unwrap();
+        frames_to_abs.push(&canny_frame);
         // let distrib_frame = gen_distribution_frame(&canny_frame, thresh1, maxval).unwrap();
 
-        let vibro_frame = compute_vibrating_pixels(&canny_frame, 4).unwrap();
-        let distrib_frame = calculate_vibrating_image(&vibro_frame, 4).unwrap();
+        // let vibro_frame = compute_vibrating_pixels(&canny_frame, 4).unwrap();
+        // let distrib_frame = calculate_vibrating_image(&vibro_frame, 4).unwrap();
 
-        highgui::imshow("window", &distrib_frame).unwrap();
+        if (frames_to_abs.len() < 10) {
+            continue;
+        }
+        let abs_image = kek(&mut frames_to_abs).unwrap();
+        // let distrib_frame = calculate_vibrating_image(&canny_frame, 4).unwrap();
+
+        highgui::imshow("window", &abs_image).unwrap();
         match highgui::wait_key(1) {
             Ok(key) => match key {
                 113 => {
