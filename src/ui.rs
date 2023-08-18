073f50562042189d66103048c2f41b5f72ea28e9
Bread White <breadrock1@gmail.com>
2023-08-18 18:13:21 +0300

    Move all highgui to own file ui.rs

---
+++ src/ui.rs
@@ -0,0 +1,50 @@
+use crate::core::cvl::CvlMat;
+use opencv::highgui::{ButtonCallback, create_button, destroy_window, imshow, named_window, wait_key};
+use opencv::highgui::{WINDOW_AUTOSIZE, WINDOW_GUI_NORMAL, QT_PUSH_BUTTON};
+
+pub enum WindowSignals {
+    CLoseApplication,
+    KeepProcessing,
+}
+
+pub struct MainWindow {
+    title: String,
+    window_flags: i32,
+}
+
+impl MainWindow {
+    pub fn new(title: &str) -> Self {
+        MainWindow {
+            title: title.to_string(),
+            window_flags: WINDOW_AUTOSIZE | WINDOW_GUI_NORMAL,
+        }
+    }
+
+    pub fn create_window(&self) {
+        let name = &self.title.as_str();
+        named_window(name, self.window_flags).unwrap();
+    }
+
+    pub fn close_window(&self) {
+        let name = &self.title.as_str();
+        destroy_window(name).unwrap();
+    }
+
+    pub fn show_frame(&self, frame: &CvlMat) {
+        let name = &self.title.as_str();
+        imshow(name, frame.frame()).unwrap();
+    }
+
+    pub fn wait_event(&self) -> WindowSignals {
+        match wait_key(10).unwrap() {
+            113 => WindowSignals::CLoseApplication,
+            _ => WindowSignals::KeepProcessing,
+        }
+    }
+
+    pub fn create_btn(&self) {
+        let callback: ButtonCallback = Some(Box::new(|val: i32| { println!("{}", val); }));
+        create_button("Test", callback, QT_PUSH_BUTTON, true)
+            .unwrap();
+    }
+}
