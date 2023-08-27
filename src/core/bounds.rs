749f0b76fa76f7e92d9ef079f6c273fd77c99436
Bread White <breadrock1@gmail.com>
2023-08-27 12:45:52 +0300

    Update `bounds.rs` after all changes

--- src/core/bounds.rs
+++ src/core/bounds.rs
@@ -1,3 +1,18 @@
+/// A red color pixel value used for marking magnitude and vibration Mat object.
+pub const RED_COLOR: (f64, f64, f64, f64) = (0.0, 0.0, 255.0, 0.0);
+
+/// A cyan color pixel value used for marking magnitude and vibration Mat object.
+pub const CYAN_COLOR: (f64, f64, f64, f64) = (255.0, 255.0, 0.0, 0.0);
+
+/// A green color pixel value used for marking magnitude and vibration Mat object.
+pub const GREEN_COLOR: (f64, f64, f64, f64) = (0.0, 255.0, 0.0, 0.0);
+
+/// A yellow color pixel value used for marking magnitude and vibration Mat object.
+pub const YELLOW_COLOR: (f64, f64, f64, f64) = (0.0, 255.0, 255.0, 0.0);
+
+/// A black color pixel value used for marking magnitude and vibration Mat object.
+pub const BLACK_COLOR: (f64, f64, f64, f64) = (0.0, 0.0, 0.0, 0.0);
+
 #[derive(Copy, Clone)]
 pub struct ColorBounds {
     channel_1: i32,
