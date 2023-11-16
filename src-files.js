81e755f2ea81c7cfaac7bf3b9d58c24690074c8c
breadrock1 <breadrock1@users.noreply.github.com>
2023-11-16 06:12:35 +0000

    Upload documentation for current release

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "cvlcore":["",[["api",[],["capture.rs","chain.rs","mod.rs"]],["core",[],["bounds.rs","deque.rs","mat.rs","mod.rs","statistic.rs"]],["ui",[],["mod.rs","window.rs"]]],["errors.rs","lib.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
